5745d032c2cc57b6b046993bf000aae44b4d8008
Alhan Keser <alhankeser@gmail.com>
2024-08-12 22:00:25 +0200

    Not pretty but it works

--- main.zig
+++ main.zig
@@ -2,7 +2,80 @@ const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 const print = std.debug.print;
 
-pub fn isInArray(haystack: [2][]const u8, needle: []const u8) bool {
+const objectNames = [2][]const u8{ "view:", "explore:" };
+const fieldNames = [6][]const u8{ "filter:", "parameter:", "dimension:", "dimension_group:", "measure:", "set:" };
+const paramNames = [62][]const u8{
+    "action:",
+    "alias:",
+    "allow_approximate_optimization:",
+    "allow_fill:",
+    "allowed_value:",
+    "alpha_sort:",
+    "approximate:",
+    "approximate_threshold:",
+    "bypass_suggest_restrictions:",
+    "can_filter:",
+    "case:",
+    "case_sensitive:",
+    "convert_tz:",
+    "datatype:",
+    "default_value:",
+    "description:",
+    "direction:",
+    "drill_fields:",
+    "end_location_field:",
+    "fanout_on:",
+    "fields:",
+    "filters:",
+    "full_suggestions:",
+    "group_item_label:",
+    "group_label:",
+    "hidden:",
+    "html:",
+    "intervals:",
+    "label:",
+    "label_from_parameter:",
+    "link:",
+    "list_field:",
+    "map_layer_name:",
+    "order_by_field:",
+    "percentile:",
+    "precision:",
+    "primary_key:",
+    "required_access_grants:",
+    "required_fields:",
+    "skip_drill_filter:",
+    "sql:",
+    "sql_distinct_key:",
+    "sql_end:",
+    "sql_latitude:",
+    "sql_longitude:",
+    "sql_start:",
+    "start_location_field:",
+    "string_datatype:",
+    "style:",
+    "suggest_dimension:",
+    "suggest_explore:",
+    "suggest_persist_for:",
+    "suggestable:",
+    "suggestions:",
+    "tags:",
+    "tiers:",
+    "timeframes:",
+    "type:",
+    "units:",
+    "value_format:",
+    "value_format_name:",
+    "view_labe:l"
+};
+
+pub fn isValidKey(keyType: i8, needle: []const u8) bool {
+    const haystack: []const []const u8 = switch (keyType) {
+        0 => &objectNames,
+        1 => &fieldNames,
+        2 => &paramNames,
+        else => return false,
+    };
     for (haystack) |thing| {
         if (std.mem.eql(u8, needle, thing)) {
             return true;
@@ -25,103 +98,58 @@ pub fn main() !void {
     const readBuf = try file.readToEndAlloc(allocator, fileSize);
     defer allocator.free(readBuf);
 
-    
-    const objectNames = [2][]const u8{ "view:", "explore:" };
-    const fieldNames = [6][]const u8{ "filter:", "parameter:", "dimension:", "dimension_group:", "measure:", "set:" };
-    const paramNames = [62][]const u8{
-        "action:",
-        "alias:",
-        "allow_approximate_optimization:",
-        "allow_fill:",
-        "allowed_value:",
-        "alpha_sort:",
-        "approximate:",
-        "approximate_threshold:",
-        "bypass_suggest_restrictions:",
-        "can_filter:",
-        "case:",
-        "case_sensitive:",
-        "convert_tz:",
-        "datatype:",
-        "default_value:",
-        "description:",
-        "direction:",
-        "drill_fields:",
-        "end_location_field:",
-        "fanout_on:",
-        "fields:",
-        "filters:",
-        "full_suggestions:",
-        "group_item_label:",
-        "group_label:",
-        "hidden:",
-        "html:",
-        "intervals:",
-        "label:",
-        "label_from_parameter:",
-        "link:",
-        "list_field:",
-        "map_layer_name:",
-        "order_by_field:",
-        "percentile:",
-        "precision:",
-        "primary_key:",
-        "required_access_grants:",
-        "required_fields:",
-        "skip_drill_filter:",
-        "sql:",
-        "sql_distinct_key:",
-        "sql_end:",
-        "sql_latitude:",
-        "sql_longitude:",
-        "sql_start:",
-        "start_location_field:",
-        "string_datatype:",
-        "style:",
-        "suggest_dimension:",
-        "suggest_explore:",
-        "suggest_persist_for:",
-        "suggestable:",
-        "suggestions:",
-        "tags:",
-        "tiers:",
-        "timeframes:",
-        "type:",
-        "units:",
-        "value_format:",
-        "value_format_name:",
-        "view_labe:l"
-    };
-    
     // var isComment = false;
     // var quotesOpen = false;
     // var quoteChar: []const u8 = "\"";
     // var isKey = true;
-    var isPreviousObjectOrField = false;
+    var isPreviousObjectKey = false;
     var isInObject = false;
     var isExpectingObjectClosingCurlyBraces = false;
-    // var isInField = false;
+    
+    var isPreviousFieldKey = false;
+    var isInField = false;
+    var isExpectingFieldClosingCurlyBraces = false;
     // var isInParameter = false;
     var it = std.mem.split(u8, readBuf, " ");
     while (it.next()) |word| {
 
         // object
-        if (!isInObject and isInArray(objectNames, word)) {
+        if (!isInObject and isValidKey(0, word)) {
             // close object
             if (isExpectingObjectClosingCurlyBraces) {
                 isExpectingObjectClosingCurlyBraces = false;
                 print("{s}", .{"\n}"});
             }
             print("\"{s}\" {{\n", .{word});
-            isPreviousObjectOrField = true;
+            isPreviousObjectKey = true;
             continue;
         }
+        if (isInObject and isValidKey(1, word)) {
+            // close field
+            if (isExpectingFieldClosingCurlyBraces) {
+                isExpectingFieldClosingCurlyBraces = false;
+                print("{s}", .{"\n}"});
+            }
+            print("  \"{s}\" {{\n", .{word});
+            isPreviousFieldKey = true;
+            continue;
+        }
+        
         // object name
-        if (isPreviousObjectOrField) {
-            print("  \"name\": \"{s}\"", .{word});
-            isPreviousObjectOrField = false;
+        if (isPreviousObjectKey) {
+            print("  \"name\": \"{s}\",\n", .{word});
+            isPreviousObjectKey = false;
             isExpectingObjectClosingCurlyBraces = true;
             isInObject = true;
+            continue;
+        }
+
+        // field name
+        if (isPreviousFieldKey) {
+            print("  \"name\": \"{s}\",\n", .{word});
+            isPreviousFieldKey = false;
+            isExpectingFieldClosingCurlyBraces = true;
+            isInField = true;
             continue;
         }
         
@@ -143,11 +171,15 @@ pub fn main() !void {
         //     isKey = true;
         // }
     }
+    if (isExpectingFieldClosingCurlyBraces) {
+        isExpectingFieldClosingCurlyBraces = false;
+        print("{s}", .{"\n}"});
+    }
     if (isExpectingObjectClosingCurlyBraces) {
         isExpectingObjectClosingCurlyBraces = false;
         print("{s}", .{"\n}"});
     }
     // _ = objectNames;
-    _ = fieldNames;
+    // _ = fieldNames;
     _ = paramNames;
 }
