9372b7d822148335a00cdd2a23b64d1cb0217182
Alhan Keser <alhankeser@gmail.com>
2024-09-18 00:48:08 +0200

    wip Start parser

--- src/main.zig
+++ src/main.zig
@@ -9,7 +9,40 @@ const TokenKind = @import("enums.zig").TokenKind;
 const ValueKind = @import("enums.zig").ValueKind;
 const Token = @import("token.zig").Token;
 const Tokenizer = @import("tokenizer.zig").Tokenizer;
+// const Parser = @import("parser.zig").Parser;
 
+pub const Parser = struct {
+    allocator: *Allocator,
+    stack: std.ArrayList(Token),
+    json: []u8,
+    
+    pub fn init(allocator: *Allocator) !Parser {
+        return Parser{
+            .allocator = allocator,
+            .stack = std.ArrayList(Token).init(allocator.*),
+            .json = &[_]u8{},
+        };
+    }
+
+    pub fn parse(self: *Parser, tokenizer: *Tokenizer) ![]u8 {
+        for (tokenizer.tokens.items) |token| {
+            try self.add(tokenizer.print_token(token));
+        }
+        try stdout.print("{s}", .{self.json});
+        return self.json;
+    }
+
+    pub fn add(self: *Parser, chars: []u8) !void {
+        const new_json = try std.fmt.allocPrint(self.allocator.*, "{s}{s}", .{self.json,chars});
+        self.allocator.free(self.json);
+        self.json = new_json;
+    }
+
+    pub fn deinit(self: *Parser) void {
+        self.allocator.free(self.json);
+        self.stack.deinit();
+    }
+};
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -30,8 +63,16 @@ pub fn main() !void {
 
     const reader = Reader.init(lkml);
     var tokenizer = try Tokenizer.init(&allocator, reader);
-    try tokenizer.tokenize();
     defer tokenizer.deinit();
+    try tokenizer.tokenize();
+    var parser = try Parser.init(&allocator);
+    defer parser.deinit();
+    _ = try parser.parse(&tokenizer);
+
+    // for (tokenizer.tokens.items) |token| {
+    //     print("{s}\n", .{tokenizer.print_token(token)});
+    // }
+
     // defer allocator.free(reader);
     // return lkml;
 }
